0f8748f130c6e0bdc2418baddc2735e8c35357a5
Delphine Martres <dmartres@gmail.com>
2018-02-28 20:50:10 +0100

    Added prod_vect method and the rest of the required tests

--- vecteur/testvecteur.cc
+++ vecteur/testvecteur.cc
@@ -50,10 +50,19 @@ int main(){
   vect3.affiche();
   cout << endl;
 
+  Vecteur vect0;
+  vect0.augmente(0);
+  vect0.augmente(0);
+  vect0.augmente(0);
+  vect3 = vect1.addition(vect0);
+  cout << "L'addition de vect1 avec le vecteur nul donne: ";
+  vect3.affiche();
+  cout << endl;
+
   Vecteur vect4;
   vect4.augmente(2.0);
   vect4.augmente(3.1);
-  vect3 = vect1.addition(vect4); // vect1 + vect2
+  vect3 = vect1.addition(vect4); // vect1 + vect4
   cout << "L'addition de vect1 et vect4 donne: ";
   vect3.affiche();
   cout << endl;
@@ -72,6 +81,11 @@ int main(){
   vect3.affiche();
   cout << endl;
 
+  vect3 = vect1.soustraction(vect1);
+  cout << "La soustraction de vect1 avec lui-même donne: ";
+  vect3.affiche();
+  cout << endl;
+
   // test oppose
   Vecteur vect6;
   vect6 = vect1.oppose();
@@ -79,12 +93,17 @@ int main(){
   vect6.affiche();
   cout << endl;
 
+  vect3 = vect1.addition(vect6);
+  cout << "L'addition de vect1 avec son opposé donne: ";
+  vect3.affiche();
+  cout << endl;
+
   // test exception set_coord
   vect1.set_coord(5,0);
 
   // test multiplication
-  Vecteur vect7(vect1.mult(2));
-  cout << "La multiplication de vect1 par 2 donne: ";
+  Vecteur vect7(vect1.mult(3));
+  cout << "La multiplication de vect1 par 3 donne: ";
   vect7.affiche();
   cout << endl;
 
@@ -93,14 +112,32 @@ int main(){
   scalaire = vect1.prod_scal(vect4); // test de l'erreur
   cout << "Produit scalaire erronné entre vect1 et vect4: " << scalaire << endl;
   cout << "Le produit scalaire de vect4 et vect5 donne: " << vect4.prod_scal(vect5) << endl;
-
+  cout << "Le produit scalaire de vect1 et vect2 donne: " << vect1.prod_scal(vect2) << endl;
+  cout << endl;
 
   // test norme et norme2.
-  double scal2;
-  scal2 = vect1.norme2();
-  cout << "La norme au carré de vect1 est " << scal2 << " unités." << endl;
-  scal2 = vect1.norme();
-  cout << "La norme de vect1 est " << scal2 << " unités." << endl;
+  double scal1;
+  scal1 = vect1.norme2();
+  cout << "La norme au carré de vect1 est " << scal1 << " unités." << endl;
+  scal1 = vect1.norme();
+  cout << "La norme de vect1 est " << scal1 << " unités." << endl;
+  cout << endl;
+
+  double scal2(vect2.norme2());
+  cout << "La norme au carré de vect2 est " << scal2 << " unités." << endl;
+  scal2 = vect2.norme();
+  cout << "La norme de vect2 est " << scal2 << " unités." << endl;
+  cout << endl;
+
+  //test produit vectoriel
+  Vecteur vect8(vect1.prod_vect(vect2));
+  cout << "Le produit vectoriel de vect1 avec vect2 donne: ";
+  vect8.affiche();
+  cout << endl;
+  Vecteur vect9(vect1.prod_vect(vect4)); // test de l'erreur
+  cout << "Produit vectoriel erronné de vect1 avec vect4: ";
+  vect9.affiche();
+  cout << endl;
 
   return 0;
 }
