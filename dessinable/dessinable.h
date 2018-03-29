a97bfdbcc202a179ad6d691864834501a5069796
Nebulean <nebuleanmusic@gmail.com>
2018-03-29 18:50:45 +0200

    Added first version of class Dessinable and its related tes (empty right now)

---
+++ dessinable/dessinable.h
@@ -0,0 +1,11 @@
+#ifndef H_DESSINABLE
+#define H_DESSINABLE
+
+class Dessinable{
+public:
+  virtual void dessine() = 0;
+
+};
+
+
+#endif // H_DESSINABLE
---
+++ dessinable/testdessinable.cc
@@ -0,0 +1,6 @@
+#include "dessinable.h"
+
+int main() {
+
+  return 0;
+}
