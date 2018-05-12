e865196611768f3e2fe5503e86d5cd7b3c0824d0
Nebulean <nebuleanmusic@gmail.com>
2018-05-12 23:44:15 +0200

    Changed f in Oscillateur.

--- exerciceP10/general/oscillateur/oscillateur.h
+++ exerciceP10/general/oscillateur/oscillateur.h
@@ -21,7 +21,7 @@ public:
    * ici, et on force la substitution dans les sous-classes. Donc cette méthode
    * est virtuelle pure.
    */
-  virtual Vecteur f(double temps) = 0;
+  virtual Vecteur f(double temps, Vecteur const& position, Vecteur const& vitesse) = 0;
 
   //! Copie polymorphique.
   virtual std::unique_ptr<Oscillateur> copie() const = 0;
