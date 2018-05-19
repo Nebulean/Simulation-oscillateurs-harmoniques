cde646e096d2defce3222e70a05b32580a2bb749
Nebulean <nebuleanmusic@gmail.com>
2018-05-19 10:02:15 +0200

    Fixed minor mistake in a Phase comment.

--- exerciceP10/general/phase/phase.h
+++ exerciceP10/general/phase/phase.h
@@ -27,13 +27,10 @@ public:
 
 private:
   //! Stocke les points de l'espace de phase.
-  /*! Liste chaînée sur une pair de vecteurs. Stocke tous les points de l'espace
+  /*! Liste chaînée sur une pair de double. Stocke tous les points de l'espace
    * de phase d'un oscillateur particulier.
    */
   std::forward_list<std::array<double, 2>> _pts;
-  // NOTE POUR DELPHINE: c'est des double qu'il faut en fait ! Tout ce qu'on veut,
-  // c'est stocker la valeur de la de P(0) et Q(0) dans le tableau, pas les
-  // vecteurs eux-mêmes ! ^^
 };
 
 #endif // H_PHASE
